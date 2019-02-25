e98b860eea5b775dc7c79e35705048bd6c976c87
constantinakondylis <constantinakondylis@192.168.1.6>
2019-02-24 20:52:33 -0700

    Third commit of cse360assign2. Filled getHistory method.

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,4 +1,6 @@
 package cse360assign2;
+import java.util.*;
+
 /*
  * Name: Constantina Kondylis
  * 
@@ -9,33 +11,73 @@ package cse360assign2;
 
 public class Calculator
 {
+	/*
+	 * data
+	 */
 	private int total;
+	private ArrayList myAL;
+	//private ArrayList ans;
 	
+	/*
+	 * constructor
+	 */
 	public Calculator () 
 	{
 		total = 0;  // not needed - included for clarity
+		myAL = new ArrayList();		//arraylist to store values and operators 
+		//ans = new ArrayList();
+		myAL.add(0);				//add 0 to the first spot in arraylist
 	}
 	
+	
+	/*
+	 * methods
+	 * 
+	 * getTotal: returns arithmetic total of all inputs
+	 */
 	public int getTotal () 
 	{
 		return total;
 	}
 	
+	/*
+	 * add: adds an input value to the total.
+	 * adds value to the arraylist
+	 */
 	public void add (int value) 
 	{
 		total = total + value;
+		myAL.add(" + ");
+		myAL.add(value);
 	}
 	
+	/*
+	 * subtract: subtracts an input value to the total.
+	 * adds the value to the arraylist
+	 */
 	public void subtract (int value) 
 	{
 		total = total - value;
+		myAL.add(" - ");
+		myAL.add(value);
 	}
 	
+	
+	/*
+	 * multiply: multiplies an input value to the total.
+	 * adds the value to the arraylist
+	 */
 	public void multiply (int value) 
 	{
 		total = total * value;
+		myAL.add(" * ");
+		myAL.add(value);
 	}
 	
+	/*
+	 * divide: divides the total by the input value parameter.
+	 * adds the value to the arraylist
+	 */
 	public void divide (int value) 
 	{
 		if(value == 0)
@@ -47,10 +89,21 @@ public class Calculator
 		{
 			total = total / value;
 		}
+		myAL.add(" / ");
+		myAL.add(value);
 	}
 	
+	/*
+	 * getHistory: iterates through arraylist containing all 
+	 * input values and operators, and prints these values and operators.
+	 * Prints new line for formatting purposes 
+	 */
 	public String getHistory () 
 	{
-		return "";
+		for (int i=0; i<myAL.size(); i++)
+			System.out.print(myAL.get(i));
+		System.out.println();
+		return null;
 	}
+	
 }
