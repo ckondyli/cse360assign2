f24a647bc6c6f84f3d9ffd5b129c19e54f823942
constantinakondylis <constantinakondylis@192.168.1.6>
2019-02-24 18:08:08 -0700

    Second commit of cse360assign2. Filled in operations methods.

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,34 +1,56 @@
 package cse360assign2;
+/*
+ * Name: Constantina Kondylis
+ * 
+ * This program acts as a calculator, adding, subtracting,
+ * multiplying, and dividing integers. It will show the history 
+ * of operations.
+ */
 
 public class Calculator
 {
 	private int total;
 	
-	public Calculator () {
+	public Calculator () 
+	{
 		total = 0;  // not needed - included for clarity
 	}
 	
-	public int getTotal () {
-		return 0;
+	public int getTotal () 
+	{
+		return total;
 	}
 	
-	public void add (int value) {
+	public void add (int value) 
+	{
+		total = total + value;
+	}
+	
+	public void subtract (int value) 
+	{
+		total = total - value;
+	}
+	
+	public void multiply (int value) 
+	{
+		total = total * value;
+	}
+	
+	public void divide (int value) 
+	{
+		if(value == 0)
+		{
+			total = 0;
+		}
 		
+		else
+		{
+			total = total / value;
+		}
 	}
 	
-	public void subtract (int value) {
-		
-	}
-	
-	public void multiply (int value) {
-		
-	}
-	
-	public void divide (int value) {
-		
-	}
-	
-	public String getHistory () {
+	public String getHistory () 
+	{
 		return "";
 	}
 }
